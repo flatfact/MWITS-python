81ddf3fcbb5d358ad8a07115d1355a74e9fb71d1
Flatfact.com <trailersubthai@gmail.com>
2018-02-19 08:50:44 +0700

    MIT licensed

--- Q_practice/test1.py
+++ Q_practice/test1.py
@@ -3,7 +3,11 @@ def p_dist(x,y,p=2):
     for i in range(len(x)):
         z = z+abs(x[i]-y[i])**p
     return z**(1/p)
-x=(3.0,6.0,1.5)
-y=(0.0,2.0,6.5)
-p=3
-print(p_dist(x,y,p))
+
+"""
+MIT License
+Copyright 2018
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
